b24ab2af9552538f6f63adb268154b1adac16e4d
Shahed Nasser <shahednasser@gmail.com>
2023-01-27 15:00:42 +0200

    make slug lower case (#20)

--- src/commands/post.ts
+++ src/commands/post.ts
@@ -2,7 +2,6 @@ import GitHubClient from "../clients/github-client"
 import Config from "../types/config";
 
 export default async function post (url: string, options: Config) {
-  
   const github = new GitHubClient(options.github, options.notion)
   await github.post(url)
 }
